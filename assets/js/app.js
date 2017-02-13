71b1a0e613320ecf3eeb28dca51b4fdec7635d39
Gareth Johns <info@garethjohnsdesign.com>
2017-02-13 18:02:07 +0000

    Add in Page Counter

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,3 +9,16 @@ $('.carousel').flickity({
   draggable: true,
   arrowShape: 'M100,46.875H11.9688l17.4688-17.4688L25,25L0,50l25,25l4.4062-4.4062L11.9688,53.125H100V46.875z'
 });
+
+if($('.carousel').length){
+var $carousel = $('.carousel').flickity();
+var $carouselStatus = $('.carousel-status');
+var flkty = $carousel.data('flickity');
+
+function updateStatus() {
+  var cellNumber = flkty.selectedIndex + 1;
+  $carouselStatus.text( cellNumber + '/' + flkty.slides.length );
+}
+updateStatus();
+$carousel.on( 'select.flickity', updateStatus );
+}
